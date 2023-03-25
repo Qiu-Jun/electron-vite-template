22765128cdc9ecbecfc5ad4fdd6aca00f3022f9c
June <1601745371@qq.com>
2023-03-25 15:56:35 +0800

    chore: 启动优化
    dev时将electron和pc分来两个启动命令

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
  * @Description:
  * @Date: 2023-03-11 00:41:24
  * @LastEditors: June
- * @LastEditTime: 2023-03-16 20:34:41
+ * @LastEditTime: 2023-03-25 15:52:59
  */
 import { defineConfig, loadEnv } from 'vite'
 import type { UserConfig, ConfigEnv } from 'vite'
@@ -14,21 +14,26 @@ import electron from 'vite-plugin-electron'
 
 export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
     const envData = loadEnv(mode, process.cwd())
-    return {
-        base: './',
-        plugins: [
-            vue(),
-            createHtmlPlugin({
-                inject: {
-                    data: {
-                        title: envData.VITE_APP_TITLE
-                    }
+    const plugins = [
+        vue(),
+        createHtmlPlugin({
+            inject: {
+                data: {
+                    title: envData.VITE_APP_TITLE
                 }
-            }),
+            }
+        })
+    ]
+    if (command === 'serve' && mode === 'develectron') {
+        plugins.push(
             electron({
                 entry: 'electron/main/index.ts'
             })
-        ],
+        )
+    }
+    return {
+        base: './',
+        plugins,
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, 'src')
