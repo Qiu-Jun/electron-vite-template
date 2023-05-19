252c2ac15bfc57e542bd38b1f8065fc7ad16a7a5
June <1601745371@qq.com>
2023-05-19 14:26:21 +0800

    fix: 处理mac端的tray问题

--- electron/main/modules/tray/index.ts
+++ electron/main/modules/tray/index.ts
@@ -4,15 +4,15 @@
  * @Author: June
  * @Date: 2023-03-13 00:57:21
  * @LastEditors: June
- * @LastEditTime: 2023-05-18 17:55:17
+ * @LastEditTime: 2023-05-19 14:22:48
  */
-import { app, Menu, Tray, nativeImage, dialog } from 'electron'
+import { app, Menu, Tray, nativeImage, dialog, BrowserWindow } from 'electron'
 import path from 'path'
 import pkg from '../../../../package.json'
 import { createWin } from '../../utils/win'
 
 const initTray = (win: any) => {
-    const iconPath = path.join(__dirname, '../..', 'public/icon.ico').replace('/\\/g', '\\\\')
+    const iconPath = path.join(__dirname, '../..', 'public/icon-tray.png').replace('/\\/g', '\\\\')
     const tray = new Tray(nativeImage.createFromPath(iconPath))
     tray.setToolTip('Mall-Cook') // 鼠标指针在托盘图标上悬停时显示的文本
     const contextMenu = Menu.buildFromTemplate([
@@ -73,6 +73,7 @@ const initTray = (win: any) => {
 
     win &&
         tray.on('click', () => {
+            if (!BrowserWindow.getAllWindows().length) return
             const winIsVisible: boolean = win.isVisible()
             // 窗口是否隐藏
             if (!winIsVisible) {
---
+++ src/pages/update/index.vue
@@ -0,0 +1,10 @@
+<!--
+ * @Author: June
+ * @Description:
+ * @Date: 2023-05-19 02:50:49
+ * @LastEditors: June
+ * @LastEditTime: 2023-05-19 14:24:54
+-->
+<template>
+    <div>update</div>
+</template>
