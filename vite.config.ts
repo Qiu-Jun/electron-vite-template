3eaea9fabc1b171a4aa5b04397402a6d6db813d8
June <1601745371@qq.com>
2023-10-28 11:42:06 +0800

    chore: pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
  * @Description:
  * @Date: 2023-03-11 00:41:24
  * @LastEditors: June
- * @LastEditTime: 2023-04-01 17:39:19
+ * @LastEditTime: 2023-10-27 20:35:55
  */
 import { defineConfig, loadEnv } from 'vite'
 import type { UserConfig, ConfigEnv } from 'vite'
@@ -11,6 +11,7 @@ import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 import { createHtmlPlugin } from 'vite-plugin-html'
 import electron from 'vite-plugin-electron'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
     const envData = loadEnv(mode, process.cwd())
@@ -22,6 +23,17 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
                     title: envData.VITE_APP_TITLE
                 }
             }
+        }),
+        AutoImport({
+            // 处理eslint 配置打开运行一次，生产后关闭
+            eslintrc: {
+                enabled: true, // Default `false`
+                filepath: './.eslintrc-auto-import.json', // Default `./.eslintrc-auto-import.json`
+                globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+            },
+
+            dts: './src/types/auto-imports.d.ts',
+            imports: ['vue', 'vue-router']
         })
     ]
     if (command === 'serve' && mode === 'develectron') {
